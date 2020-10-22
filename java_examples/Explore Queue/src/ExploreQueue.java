0a76a7203a8f0ccebfd35401d4c9160e25f282dd
Alex Swain <lexxas@live.com>
2020-10-22 18:21:22 -0400

    fourth commit

--- java_examples/Explore Queue/src/ExploreQueue.java
+++ java_examples/Explore Queue/src/ExploreQueue.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -6,7 +7,19 @@ public class ExploreQueue {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Queue<Integer> q = new LinkedList<Interger>();
+		Queue<Integer> i = new LinkedList<Integer>();
+		Integer ten = 10;
+		i.add(ten);
+		i.add(7);
+		i.add(ten);
+		i.add(98);
+		i.add(64);
+		i.add(1);
+		i.add(79);
+		
+		i.remove(ten);
+		
+		i.
 	}
 
 }
